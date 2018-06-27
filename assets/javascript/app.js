a4612abe4e3bd3eebc1648e8c19687e7054a9b90
Mandy <mandyjjoles@gmail.com>
2018-06-26 20:47:28 -0500

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var score = 0
+
+//for(var i=0; i < questions.length; i++) {
+   //// var response = quest
+//}
+
+var questions = $(".first, .second, .third, .fourth");
+console.log("p content: "+document.getElementById(questions[0].innerHTML));
+//var userGuesss = function() {
+   // $(".correctOne").on("click", stopwatch.recordLap);
